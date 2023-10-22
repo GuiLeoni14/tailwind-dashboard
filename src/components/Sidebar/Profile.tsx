e4d96384000bf1bba89bf393d0c723c0dac416e6
GuiLeoni14 <guileone167@gmail.com>
2023-10-22 13:48:08 -0300

    feat: layout

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -1,24 +1,27 @@
 import { LogOut } from 'lucide-react'
+import { Button } from '../Button'
+
+export interface ProfileProps {}
 
 export function Profile() {
   return (
-    <div className="grid-cols-profile grid items-center gap-3">
+    <div className="flex items-center gap-3">
       <img
         src="https://github.com/diego3g.png"
         className="h-10 w-10 rounded-full"
         alt=""
       />
-      <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">
+      <div className="flex flex-col">
+        <span className="block text-sm font-semibold text-zinc-700 dark:text-zinc-100">
           Diego Fernandes
         </span>
-        <span className="truncate text-sm text-zinc-500">
-          diego@rocketseat.com.br
+        <span className="block text-sm text-zinc-500 dark:text-zinc-400">
+          diego.schell.f@gmail.com
         </span>
       </div>
-      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
-        <LogOut className="h-5 w-5 text-zinc-500" />
-      </button>
+      <Button variant="ghost" className="ml-auto">
+        <LogOut className="h-5 w-5 text-zinc-500 dark:text-zinc-400" />
+      </Button>
     </div>
   )
 }
